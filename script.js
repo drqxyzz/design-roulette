9a5b74b3998e9db29d362b2a1b27bbfa1ca0b021
drq <117029096+drqxyzz@users.noreply.github.com>
2024-08-20 14:39:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
 const businessTypes = [
     "Coffee Shop", "Book Store", "Art Gallery", "Tech Store", "Grocery Market", "Toy Shop",
     "Flower Boutique", "Clothing Store", "Music Studio", "Bakery", "Spa", "Fitness Center",
-    "Pet Store", "Jewelry Store", "Furniture Store", "Cinema", "Restaurant", "Ice Cream Parlor",
-    "Library", "Record Store", "Tattoo Studio", "Barber Shop", "Photography Studio", "Book Cafe",
-    "Comic Store", "Garden Center", "Beauty Salon", "Antique Shop", "Boutique Hotel", "Farmers Market",
-    "Candle Shop", "Ceramics Studio", "Dance Studio", "Art Supply Store", "Stationery Shop", "Fabric Store",
-    "Wine Cellar", "Organic Grocery", "Shoe Store", "Mobile Repair", "Game Store", "Music School",
-    "Language School", "Cooking School", "Candy Store", "Bicycle Shop", "Watch Store", "Perfume Shop",
-    "Optician", "Gift Shop", "Nail Salon", "Fashion Boutique"
+    "Pet Store", "Jewelry Store", "Furniture Store", "Cinema", "Restaurant", "Ice Cream Parlor"
 ];
 
 const themes = [
     "Whimsical Wonderland", "Retro Revival", "Boho Chic", "Underwater Utopia", "Mystical Forest", "Space Odyssey",
     "Steampunk Dreams", "Enchanted Garden", "Tropical Escape", "Urban Jungle", "Rustic Charm", "Futuristic Fantasy",
-    "Vintage Vibes", "Summer Breeze", "Fairy Tale", "Desert Mirage", "Winter Wonderland", "Pirate's Cove",
-    "Magical Kingdom", "Cyberpunk City", "Medieval Times", "Carnival Craze", "Candy Land", "Forest Retreat",
-    "Autumn Harvest", "Sci-Fi Adventure", "Mountain Escape", "Seaside Serenity", "Galaxy Exploration", "Art Nouveau",
-    "Pop Art Explosion", "Circus Funhouse", "Haunted Mansion", "Safari Adventure", "Renaissance Fair", "Wild West",
-    "Jungle Safari", "Island Getaway", "Gothic Glamour", "Vintage Circus", "Celestial Skies", "Zen Garden",
-    "Mystery Manor", "Aquatic Paradise", "Enchanted Forest", "Fantasy Fair", "Moonlit Night", "Sunset Bliss",
-    "Fire and Ice", "Cloud 9"
+    "Vintage Vibes", "Summer Breeze", "Fairy Tale", "Desert Mirage", "Winter Wonderland", "Pirate's Cove"
 ];
 
 const primaryColors = [
     "Baby Blue", "Salmon Pink", "Mint Green", "Lavender", "Ivory", "Peach",
     "Champagne", "Navy Blue", "Teal", "Coral", "Magenta", "Lemon Yellow",
-    "Rose Gold", "Emerald Green", "Crimson Red", "Aqua", "Mustard Yellow", "Plum",
-    "Olive Green", "Charcoal", "Fuchsia", "Sky Blue", "Burgundy", "Cobalt Blue",
-    "Blush Pink", "Forest Green", "Ruby Red", "Sapphire Blue", "Amethyst", "Turquoise",
-    "Burnt Orange", "Mauve", "Caramel", "Pearl", "Maroon", "Hazel",
-    "Lilac", "Cyan", "Grape", "Copper", "Silver", "Gold",
-    "Indigo", "Amber", "Vermilion", "Saffron", "Chocolate", "Pistachio",
-    "Lime Green", "Slate Gray"
+    "Rose Gold", "Emerald Green", "Crimson Red", "Aqua", "Mustard Yellow", "Plum"
 ];
 
 const fonts = [
@@ -39,23 +21,13 @@ const fonts = [
     "Trebuchet MS", "Impact", "Gill Sans", "Futura", "Garamond", "Palatino",
     "Baskerville", "Didot", "Rockwell", "Helvetica", "Lucida Console", "Copperplate",
     "Brush Script", "Franklin Gothic", "Book Antiqua", "Century Gothic", "Merriweather",
-    "Montserrat", "Raleway", "Lobster", "Playfair Display", "Bebas Neue", "Open Sans",
-    "Roboto", "Lato", "Oswald", "Poppins", "Nunito", "Quicksand",
-    "Source Sans Pro", "Comfortaa", "Pacifico", "Exo", "Righteous", "Dancing Script",
-    "Kalam", "Satisfy", "Great Vibes", "Caveat", "Inconsolata", "Zilla Slab",
-    "Muli", "PT Sans", "Russo One", "Play"
+    "Montserrat", "Raleway", "Lobster", "Playfair Display", "Bebas Neue"
 ];
 
 const illustrationStyles = [
     "Realistic", "Abstract", "Cartoon", "Flat Design", "Line Art", "Minimalist",
     "Watercolor", "Pixel Art", "Chalk Drawing", "Comic Book", "Silhouette", "Graffiti",
-    "Pop Art", "3D Render", "Charcoal", "Ink Wash", "Collage", "Sketch",
-    "Vintage Poster", "Neon Glow", "Stencil", "Oil Painting", "Acrylic", "Pastel",
-    "Manga", "Art Deco", "Impressionist", "Cubist", "Surrealist", "Doodle",
-    "Pencil Sketch", "Woodcut", "Vector Art", "Low Poly", "Digital Painting", "Retro Comic",
-    "Line Drawing", "Patterned", "Geometric", "Photorealism", "Airbrush", "Stipple",
-    "Continuous Line", "Scribble", "Crayon", "Spray Paint", "Tattoo Style", "Claymation",
-    "Street Art", "Mixed Media"
+    "Pop Art", "3D Render", "Charcoal", "Ink Wash", "Collage", "Sketch"
 ];
 
 const categories = [
